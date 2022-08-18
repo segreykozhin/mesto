61ad49dac38885131d4c15d8540277544f30e981
Sergey Kozhin <sergey444555@gmail.com>
2022-08-18 08:56:32 +0300

    мой третий коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,37 @@
 const popup = document.querySelector('.popup');
-const openButton = document.querySelector('.profile__edit-button');
-const closeButton = popup.querySelector('.popup__close');
-const saveButton = document.querySelector('.popup__button');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const profileCloseButton = popup.querySelector('.popup__close');
+const popupSaveButton = document.querySelector('.popup__button');
 let popupForm = document.querySelector('.popup__form');
-const togglePopup = function() {
-  popup.classList.toggle('popup_opened');
+const nameInput = document.querySelector('.popup__input_data_name');
+const workInput = document.querySelector('.popup__input_data_work');
+let profileTitle = document.querySelector('.profile__title');
+let profileCaption = document.querySelector('.profile__caption');
+
+const openPopup = function() {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  workInput.value = profileCaption.textContent;
+}
+
+const closePopup = function() {
+  popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  const nameInput = document.querySelector('.popup__input-name').value;
-  const workInput = document.querySelector('.popup__input-work').value;
 
-  let profileTitle = document.querySelector('.profile__title');
-  let profileCaption = document.querySelector('.profile__caption');
+  const nameInput = document.querySelector('.popup__input_data_name').value;
+  // если перенести к остальным переменным, то функция не работает
+  const workInput = document.querySelector('.popup__input_data_work').value;
 
   profileTitle.textContent = nameInput;
   profileCaption.textContent = workInput;
+  closePopup();
 }
+
 popupForm.addEventListener('submit',formSubmitHandler);
-openButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+profileEditButton.addEventListener('click', openPopup);
+profileCloseButton.addEventListener('click', closePopup);
 
 
